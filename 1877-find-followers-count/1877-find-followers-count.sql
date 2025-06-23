8381c5befa6ac3df726b064e3d01427e3d031799
Himanshu Kumar <118829051+Himanshu12211967@users.noreply.github.com>
2025-06-23 09:43:50 +0530

    Time: 640 ms (15.7%), Space: 0B (100%) - LeetHub

--- 1877-find-followers-count/1877-find-followers-count.sql
+++ 1877-find-followers-count/1877-find-followers-count.sql
@@ -1,5 +1,5 @@
-select user_id,
-count(*) as followers_count
-from Followers
-group by user_id
-order by user_id;
+select f1.user_id,count(*)/(select distinct count(*) from Followers where user_id = f1.user_id) as followers_count
+from Followers f1 join Followers f2
+where f1.user_id = f2.user_id
+group by f1.user_id
+order by f1.user_id;
